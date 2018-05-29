5dcef0ff1c9dba3f60241cba085763fdc1f34198
Ben Hadfield <benjohnhadfield@gmail.com>
2018-05-29 18:49:40 +0100

    vpo/vpn fn implemented

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod hardware;
+mod address;
 
 fn main() {
-    let mem = hardware::Memory::new(2, 12, 14);
-    println!("Hello, world!");
+    // create memory model
+    let mm = hardware::Memory::new(2, 12, 14);
+    // dummy virtual addr
+    let va: u64 = 13;
+    // get the vpo
+    let vpo = address::vpo
 }
